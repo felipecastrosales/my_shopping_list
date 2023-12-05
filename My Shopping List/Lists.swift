47d607a8e483c546696fe859eef50d394879f48d
Felipe Sales <soufeliposales@gmail.com>
2023-12-05 20:53:36 -0300

    chore: var and let

--- My Shopping List/Lists.swift
+++ My Shopping List/Lists.swift
@@ -9,7 +9,17 @@ import Foundation
 import UIKit
 
 class Lists:UIViewController {
+    var v1 = 3;
+    var v2 = 5;
+    var v3 = 9;
+    var string1 = "Hello, World!"
+
     override func viewDidLoad() {
+        v1 = v2 + v1;
+        var v4 = 8;
+        print(v1);
+        print(string1)
         
+        v3 = 2
     }
 }
