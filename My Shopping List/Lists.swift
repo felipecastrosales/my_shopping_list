a5c2c07d01a136c4962f90867a302ed90c2dbc5e
Felipe Sales <soufeliposales@gmail.com>
2023-12-05 20:49:10 -0300

    chore: classes and delegates

---
+++ My Shopping List/Lists.swift
@@ -0,0 +1,15 @@
+//
+//  Lists.swift
+//  My Shopping List
+//
+//  Created by Sua Musica on 05/12/23.
+//
+
+import Foundation
+import UIKit
+
+class Lists:UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
