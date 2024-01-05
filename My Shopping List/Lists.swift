75683e7d2eff5dc6c203016decdf63212f117746
Felipe Sales <soufeliposales@gmail.com>
2024-01-04 21:52:12 -0300

    chore: dictionaries and arrays

--- My Shopping List/Lists.swift
+++ My Shopping List/Lists.swift
@@ -21,6 +21,88 @@ class Lists:UIViewController {
         print(v1);
         print(string1)
         NSLog("Olá, %@, %i, %f", string1, v1, vPi)
+        
+        if (v2 == 5) {
+            NSLog("v2 is 5");
+        }
+
+        if (v1 < v3) {
+            NSLog("less tan v3");
+        } else if (v1 == 10) {
+            NSLog("is equal to 10");
+        } else {
+            NSLog("is greather than ");
+        }
+        
         v3 = 2
+        
+        ///
+        
+        let myArray = ["a", "b", "c"];
+//        var arrayNs = NSArray()
+        var arrayNs = NSMutableArray()
+        var arrayMutable = NSMutableArray()
+        
+        arrayNs.add("felipe")
+        arrayNs.add("castro")
+        arrayNs.add("sales")
+        var selectedIndex = 0
+        
+        
+        print(arrayNs)
+        
+//        arrayNs.removeObject(at: 1)
+//        print(arrayNs)
+        
+        for i in 0..<arrayNs.count {
+            let element = arrayNs[i] as? String
+            if (element == "castro") {
+                selectedIndex = i
+            }
+        }
+        
+        arrayNs.removeObject(at: selectedIndex)
+
+        print(arrayNs)
+        
+        arrayNs.replaceObject(at: selectedIndex, with: "santos")
+        
+        print(arrayNs)
+
+        for i in myArray {
+            NSLog("Olá, %@", i);
+            if (i == "a") {
+                NSLog("Olá when is a, %@", i);
+            }
+        }
+        
+        // for x in 0...v2 {
+        // for x in 0..<v2 {
+        for x in 0..<myArray.count {
+            print(x);
+        }
+        
+//        var dic1 = NSDictionary();
+        var dic1 = NSMutableDictionary()
+//        dic1.setValue("felipe", forKey: "name")
+        dic1["name"] = "felipe"
+        dic1["age"] = 20
+        dic1["letters"] = myArray
+        
+        arrayMutable.add(dic1)
+        arrayMutable.add(dic1)
+        
+        print(arrayMutable)
+//        let nameVar:String = dic1["nome"] as! String
+        let nameVar = dic1["name"] as! String
+
+        print(dic1)
+        print(dic1["name"])
+        print(nameVar)
+        
+        dic1.removeObject(forKey: "name")
+        print(dic1)
+        print(dic1["name"])
+        print(nameVar)
     }
 }
